f3a282e5f94f18448376a2125088034154a5c797
LIAM-K909 <83389172+LIAM-K909@users.noreply.github.com>
2021-11-30 19:13:34 +0530

    Add files via upload

---
+++ app_page.js
@@ -0,0 +1,14 @@
+var firebaseConfig = 
+{
+    apiKey: "AIzaSyC8Gz7InwdP1Ezb31jEA45_CkoOWAj06XI",
+    authDomain: "project-94-lets-chat-app-cf968.firebaseapp.com",
+    projectId: "project-94-lets-chat-app-cf968",
+    storageBucket: "project-94-lets-chat-app-cf968.appspot.com",
+    messagingSenderId: "124958463794",
+    appId: "1:124958463794:web:f6eb42d3f84585b4d1a92b"
+};
+
+//Initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name
